9682f9afc0a8705083c35296fdc525155ca5c25a
olof <aksberg.olof@outlook.com>
2022-06-25 16:26:32 +0200

    d

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 (async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI);
+ try {
+  await mongoose.connect(process.env.DB_URI);
 
-    console.log("mongo connected");
-  } catch (err) {
-    process.exit();
-  }
+  console.log("mongo connected");
+ } catch (err) {
+  process.exit();
+ }
 })();
 
 const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
+ origin: "*",
+ credentials: true,
+ optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
@@ -44,15 +44,15 @@ app.use(express.static(path.resolve("./omc/public")));
 app.use("/", index);
 
 app.get("*", (req, res) =>
-  res.sendFile("index.html", {
-    //   dev
-    root: "./omc/public",
+ res.sendFile("index.html", {
+  //   dev
+  root: "./omc/public",
 
-    //   prod
-    //    root: "./omc/build"
-  })
+  //   prod
+  //    root: "./omc/build"
+ })
 );
 
 app.listen(PORT, () => {
-  console.log("lyssnar" + PORT);
+ console.log("lyssnar port " + PORT);
 });
