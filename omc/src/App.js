4e925ac53731e11096067bcc798ef5dc67c73258
olof <aksberg.olof@outlook.com>
2022-06-26 12:10:17 +0200

    small fixes

--- omc/src/App.js
+++ omc/src/App.js
@@ -12,31 +12,30 @@ import { getTopScores, scoresData } from "./store/scoresSlice.js";
 import "./style/main.scss";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const [gameAuth, setGameAuth] = useState(false);
-  const { status } = useSelector(scoresData);
+ const dispatch = useDispatch();
+ const [gameAuth, setGameAuth] = useState(false);
+ const { status } = useSelector(scoresData);
 
-  useEffect(() => {
-    if (status === "idle") {
-      dispatch(getTopScores("page=1&limit=10"));
-    }
-  }, [status, dispatch]);
+ useEffect(() => {
+  if (status === "idle") {
+   dispatch(getTopScores("page=1&limit=10"));
+  }
+ }, [status, dispatch]);
 
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route element={<Home setGameAuth={setGameAuth} />} path="/" exact />
-          <Route
-            element={gameAuth ? <Game /> : <Home setGameAuth={setGameAuth} />}
-            path="/game"
-            exact
-          />
-          <Route element={<Home setGameAuth={setGameAuth} />} path="*" />
-        </Routes>
-      </Router>
-    </div>
-  );
+ return (
+  <div className="App">
+   <Router>
+    <Routes>
+     <Route element={<Home setGameAuth={setGameAuth} />} path="/" />
+     <Route
+      element={gameAuth ? <Game /> : <Home setGameAuth={setGameAuth} />}
+      path="/game"
+     />
+     <Route element={<Home setGameAuth={setGameAuth} />} path="*" />
+    </Routes>
+   </Router>
+  </div>
+ );
 };
 
 export default App;
